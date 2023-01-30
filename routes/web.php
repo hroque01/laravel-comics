297ea196c71975a69113b4d5cd164dbeff82b11b
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-01-30 18:53:48 +0100

    Add images

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Prodotti');
 });
+
+Route::get('/prodotto', function () {
+    return view('Prodotto');
+});
+
